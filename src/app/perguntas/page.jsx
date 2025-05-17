2b0eab1a76290e0013ec99d2e7bf683357bdc467
Pedro Luiz <pedrinholos11@gmail.com>
2025-05-16 21:41:31 -0300

    useEffect na resposta

--- src/app/perguntas/page.jsx
+++ src/app/perguntas/page.jsx
@@ -1,9 +1,15 @@
 "use client"
 import { useForm } from "@/context/FormContext"
+import { useEffect, useState } from "react"
 
 export default function Resultado() {
   const { respostas,aiResponse } = useForm()
-  const RespostaLocalStorange = localStorage.getItem("resposta")
+  const [RespostaLocalStorange,setRespostaLocalStorange] = useState("")
+  useEffect(() => {
+    const Resposta = localStorage.getItem("resposta")
+    setRespostaLocalStorange(respostas)
+  },[aiResponse])
+  
   return (
     <div>
       <p>{RespostaLocalStorange}</p>
